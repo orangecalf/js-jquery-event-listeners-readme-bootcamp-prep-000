17b8b2807b87fdce3be33d7051aef782dd7eca84
yossi katzman <issoyk@gmail.com>
2018-03-19 18:36:48 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -26,7 +26,7 @@ function pressIt() {
 }
 
 function submitIt() {
-  $('input:last-child').on('submit', function() {
+  $('input').on('submit', function() {
     alert("Your form is going to be submitted now.")
   })
 }
