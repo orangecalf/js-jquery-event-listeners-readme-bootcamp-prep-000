5b4d49e97472a068e92bc0f039acb4c0db02f042
yossi katzman <issoyk@gmail.com>
2018-03-19 18:38:53 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,30 @@
 $(document).ready(function(){
 
 // call functions here
-
 });
+
+function getIt() {
+  $('p').on("click", function() {
+  alert("Hey!")
+})
+  
+}
+
+function frameIt() {
+  $("img").on("load", function() {
+    $('img').addClass('tasty')
+  })
+}
+function pressIt() {
+  $("#typing").on("keydown", function(e) {
+    if (e.which === 71) {
+      alert("g")
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.")
+  })
+}
